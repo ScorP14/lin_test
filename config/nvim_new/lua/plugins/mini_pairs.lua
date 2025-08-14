7cd43ae0775455e07cb053279123e311307a25b3
ScorP14Qtile <VB@scorp.com>
2025-08-14 19:01:57 +0800

    edit config mini.pairs

--- config/nvim_new/lua/plugins/mini_pairs.lua
+++ config/nvim_new/lua/plugins/mini_pairs.lua
@@ -1,1 +1,8 @@
-require("mini.pairs").setup({})
+require("mini.pairs").setup({
+  -- opts = {}
+  modes = { insert = true, command = true, terminal = false },
+  skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+  skip_ts = { "string" },
+  skip_unbalanced = true,
+  markdown = true,
+})
