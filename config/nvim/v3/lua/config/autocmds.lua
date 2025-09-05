e1ec9e5d3cdc0f4436993bf781f1e0da384c3bcd
ScorP14 <NoteBook@Arch>
2025-09-05 10:36:50 +0800

    refactoring nvim v3 config

--- config/nvim/v3/lua/config/autocmds.lua
+++ config/nvim/v3/lua/config/autocmds.lua
@@ -34,3 +34,13 @@ autocmd("TextYankPost", {
   end,
   desc = "Highlight text on yank",
 })
+
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = "*.py",
+  callback = function()
+    vim.lsp.buf.format()
+    vim.lsp.buf.code_action({context = {only = {"source.organizeImports"}}, apply = true})
+  end
+})
+
