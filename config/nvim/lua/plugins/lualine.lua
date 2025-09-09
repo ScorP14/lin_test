10a707738f436397c77d906a0677a4d1a995b689
ScorP14Qtile <VB@scorp.com>
2025-09-09 22:56:23 +0800

    nvim-config-plugin-lualine edit

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,21 @@
 return {
-	lazy = false,
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = "auto",
-		},
-	},
+  lazy = false,
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "dracula", --dracula, gruvbox, onedark, tokyonignt
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch' },
+      lualine_c = { 'filename', 'diff', 'lsp_status' },
+      lualine_x = { 'diagnostics' },
+      lualine_y = { 'filetype' },
+      lualine_z = { 'progress', 'location' }
+    }
+  },
 }
